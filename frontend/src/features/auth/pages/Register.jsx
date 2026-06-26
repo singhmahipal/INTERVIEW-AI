08f8c06101750131c860b7af5fafd7c2cf81790b
singhmahipal <mahipalsingh052025@gmail.com>
2026-06-26 15:27:20 +0530

    implement custom hook (useAuth) for register with 2-way-binding

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,9 +1,71 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router";
+import "../auth.form.scss";
+import { useAuth } from "../hooks/useAuth.js";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const {loading, handleRegister} = useAuth();
+  const navigate = useNavigate();
 
-export default Register
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await handleRegister({username, email, password});
+    navigate("/");
+  };
+
+  if (loading) {
+    return (
+      <main><h1>Loading...</h1></main>
+    )
+  }
+
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Register</h1>
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+            onChange={(e) => {setEmail(e.target.value)}}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter email address"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="username">Username</label>
+            <input
+            onChange={(e) => {setUsername(e.target.value)}}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter username"
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+            onChange={(e) => {setPassword(e.target.value)}}
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Enter password"
+            />
+          </div>
+          <button className="button primary-button">Register</button>
+        </form>
+        <p>
+          Already have an account? <Link to={"/login"}>Login</Link>
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
