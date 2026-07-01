0cd3fe36a906f17c4796ac2d517d5647ed0e4ca5
singhmahipal <mahipalsingh052025@gmail.com>
2026-07-01 22:59:37 +0530

    implement interview api layer

---
+++ frontend/src/features/interview/services/interview.api.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "http://localhost:3000",
+  withCredentials: true,
+});
+
+/**
+ * @description Service to generate interview report based on user self description, resume and job description.
+ */
+export const generateInterviewReport = async ({
+  jobDescription,
+  selfDescription,
+  resumeFile,
+}) => {
+  const formData = new formData();
+  formData.append("jobDescription", jobDescription);
+  formData.append("selfDescription", selfDescription);
+  formData.append("resume", resumeFile);
+
+  const response = await api.post("/api/interview/", formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+
+  return response.data;
+};
+
+/**
+ * @description Service to get interview report by interviewId.
+ */
+export const getInterviewReportById = async ({ interviewId }) => {
+  const response = await api.get(`/api/interview/report/${interviewId}`);
+
+  return response.data;
+};
+
+/**
+ * @description Service to get all interview reports of logged in user.
+ */
+export const getAllInterviewReports = async () => {
+  const response = await api.get("/api/interview/");
+
+  return response.data;
+};
